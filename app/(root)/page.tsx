e6a358edead36608ecc31ed9416b5b0f7ea3388e
sriram N V <sriramnvilvanathan@gmail.com>
2025-04-16 20:50:46 +0530

    added themes and fonts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,5 @@
-import Footer from "../components/Footer";
-
-
 export default function Home() {
   return( <>
-        <h1 className="text-3xl">Home</h1>
-        
-        <Footer/>
+        <h1 className="text-3xl">Home</h1>    
       </>);    
 }
