cee3ffa118d373deec75240d50262e063f2f4907
sriram N V <sriramnvilvanathan@gmail.com>
2025-04-21 12:51:28 +0530

    completed authentication using github OAuth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
+import { auth, signIn, signOut } from '@/auth';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
-    <div className='bg-white text-black px-5 py-3'>Navbar</div>
+    <div className='bg-white px-5 py-3 shodow-sm font-work-sans'>
+      <nav className='flex justify-between items-center gap-5'>
+        <Link href='/'>
+          <Image src='/logo.png' alt='logo' width={144} height={30} />
+        </Link>
+
+        <div className='flex items-center gap-5 text-black'>
+          {session && session?.user ? (
+              <>
+                <Link href="/startup/create">
+                  <span>Create</span>
+                </Link>
+                <form action={async () => {
+                  "use server";
+                  await signOut({redirectTo: '/'});
+                  }}>
+                    <button type='submit'>Sign Out</button>
+                </form>
+
+                <Link href={`/user/${session?.id}`}>
+                  <span>{session?.user?.name}</span>
+                </Link>
+              </>
+            ):(
+              <>
+                <form action={async () => {
+                  "use server"
+                  await signIn('github');
+                }}>
+                  <button type='submit'>Log In</button>
+                </form>
+              </>
+            )}
+        </div>
+      </nav>
+    </div>
   )
 }
 
